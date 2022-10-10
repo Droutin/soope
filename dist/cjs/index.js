01c1573e8590003fe0281d8654748f637c5e05d4
Roman Vašek <rvasek0@gmail.com>
2022-10-10 10:28:50 +0200

    removed forgoten console.log

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -515,7 +515,6 @@ class Soope {
         await this.autoImport(this.dirs.routes, (Route, className, filePath, dir) => {
             const route = new Route();
             const handlers = (0, utils_1.getClassMethods)(route);
-            console.log(handlers);
             const path = this.buildPath(dir, filePath, route?.path);
             if (route?.crud) {
                 (0, utils_1.entries)(this.cruds)
